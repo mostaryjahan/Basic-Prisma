9f806de5f2ffcb6f8ad98b513e991fdc73dcdd3a
Mostary Jahan <155467590+mostaryjahan@users.noreply.github.com>
2025-09-21 13:56:49 +0600

    searching, sorting

--- Hello-prisma/src/sorting.ts
+++ Hello-prisma/src/sorting.ts
@@ -3,26 +3,48 @@ import { PrismaClient } from "../generated/prisma";
 const prisma = new PrismaClient();
 
 async function main() {
-    const insertsUser = await prisma.user.createMany({
-       data:[
-        {
-            name: "js",
-            email: "ts@email.com"
-        },
-        {
-            name: "rain",
-            email: "rain@email.com"
-        },
-        {
-            name: "tut tut",
-            email: "ts@email.com"
-        },
+    // const insertsUser = await prisma.user.createMany({
+    //    data:[
+    //     {
+    //         name: "js",
+    //         email: "ts@email.com"
+    //     },
+    //     {
+    //         name: "rain",
+    //         email: "rain@email.com"
+    //     },
+    //     {
+    //         name: "tut tut",
+    //         email: "ts@email.com"
+    //     },
         
         
         
-       ]
+    //    ]
+    // });
+    // console.log(insertsUser);
+
+
+    // const users = await prisma.user.findMany({
+    //     orderBy:{
+    //         id: "asc"
+    //     }
+    // });
+
+       const users = await prisma.user.findMany({
+        where:{
+            name:{
+                contains: "t",
+                mode: "insensitive"
+            }
+        },
+        orderBy:{
+            name:"asc"
+        }
     });
-    console.log(insertsUser);
+    console.log(users);
+
+
 }
 
 main();
